8cacb5d9233f5ab60b1de28c942742dce6695448
Surabhi-M-R <surabhimr27@gmail.com>
2025-06-08 00:09:30 +0530

    THIRD COMMIT

--- task.js
+++ task.js
@@ -9,23 +9,33 @@ const eventCounts = {
 };
 
 emitter.on("user-login", (username) => {
+    eventCounts["user-login"]++;
     console.log(`${username} logged in!`);
+
 });
 
 emitter.on("user-purchase", (username, item) => {
+    eventCounts["user-purchase"]++;
     console.log(`${username} purchased ${item}`);
 });
 
 emitter.on("profile-update", (username, field) => {
+    eventCounts["profile-update"]++;
     console.log(`${username} updated their ${field}`);
 });
 
 emitter.on("user-logout", (username) => {
+    eventCounts["user-logout"]++;
     console.log(`${username} logged out`);
 });
+emitter.on("summary",()=>{
+    console.log(eventCounts);
+})
 
 
 emitter.emit("user-login", "sush");
 emitter.emit("user-purchase", "sush", "laptop");
 emitter.emit("profile-update", "sush", "email");
 emitter.emit("user-logout", "sush");
+
+emitter.emit("summary");
